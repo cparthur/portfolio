7e3c4fbe10394cfed6d8a9afbfc994fcc9ef0eb8
Arthur <a@skatekrak.com>
2020-02-05 13:09:44 +0000

    :sparkles: Add fluid typography + nunito font

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,17 +1,35 @@
 import { createGlobalStyle } from 'styled-components';
 
+import media, { Sizes } from 'styles/media';
 import reset from './reset';
+
+const minFs = '16';
+const maxFs = '20';
 
 export default createGlobalStyle`
     ${reset}
     
-    @font-face {
+    /* @font-face {
         font-family: 'custom-font';
         src: url('#') format('woff');
+    } */
+
+    /* Fluid typography */
+    html {
+        font-size: ${`${minFs}px`};
+
+        ${media.mobileM} {
+            font-size: ${`calc(${minFs}px + (${maxFs} - ${minFs}) * ((100vw - ${Sizes.mobileS}px) / (${Sizes.laptopL} - ${Sizes.mobileS})))`};
+        }
+
+        ${media.laptopL} {
+            font-size: ${`${maxFs}px`};
+        }
     }
 
-    /* Custom here */
-    body {
-        font-size: 16px;
+    /* Custom styles*/
+    h1 {
+        font-family: 'nunito', sans-serif;
+        font-weight: 400;
     }
 `;
