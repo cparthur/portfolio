bd69d1bf7b9c1c9716948d367dbe4b8010d245d1
Arthur <a@skatekrak.com>
2020-02-05 13:10:18 +0000

    :sparkles: Align theme with figma design

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,63 +1,42 @@
 const light = {
-    bg: {
-        primary: '#eff0f5',
-        secondary: '#ffffff',
-        inset: '#e2e4e8',
-        input: 'rgba(65,67,78,0.12)',
-    },
+    black: '#000000',
+    white: '#ffffff',
     text: {
-        primary: '#050505',
-        secondary: '#2f3037',
-        tertiary: '#525560',
-        quarternary: '#9194a1',
-        placeholder: 'rgba(82,85,96,0.5)',
-        onPrimary: '#ffffff',
+        prim: '#504830',
+        sec: '#9F8F60',
+        ter: '#B8AC89',
+        quar: '#D1C9B2',
     },
-    // ...
-};
-
-const dark = {
     bg: {
-        primary: '#050505',
-        secondary: '#111111',
-        inset: '#111111',
-        input: 'rgba(191,193,201,0.12)',
+        prim: '#F9F9F5',
+        sec: '#E2E2CF',
     },
-    text: {
-        primary: '#fbfbfc',
-        secondary: '#e3e4e8',
-        tertiary: '#a9abb6',
-        quarternary: '#6c6f7e',
-        placeholder: 'rgba(145,148,161,0.5)',
-        onPrimary: '#050505',
+    highlight: {
+        gradient: 'linear-gradient(90deg, #FF9500 0%, #CC2500 100%)',
     },
-    // ...
 };
 
 const defaultTheme = {
+    // Major Second modular scale system
     fontSizes: [
-        '14px', // 0
-        '16px', // 1
-        '18px', // 2
-        '22px', // 3
-        '26px', // 4
-        '32px', // 5
-        '40px', // 6
+        '0.79rem', // 0 -> 12.64px
+        '0.889rem', // 1 -> 14.22px
+        '1rem', // 2 -> 16px
+        '1.125rem', // 3 -> 18px
+        '1.266rem', // 4 -> 20.25px
+        '1.424rem', // 5 -> 22.78px
+        '1.602rem', // 6 -> 25.63px
+        '1.802rem', //  -> 28.83px
     ],
     fontWeights: {
-        body: 400,
-        subheading: 500,
-        link: 600,
-        bold: 700,
-        heading: 800,
+        light: 300,
+        regular: 400,
+        bold: 600,
     },
     lineHeights: {
-        body: 1.5,
+        body: 1.45,
         heading: 1.3,
-        code: 1.6,
     },
-    // ...
 };
 
 export const lightTheme = { ...defaultTheme, ...light };
-export const darkTheme = { ...defaultTheme, ...dark };
