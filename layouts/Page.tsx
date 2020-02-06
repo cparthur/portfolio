676bc0642e7c5e3f31e80e9953be28cdae6c95dc
Arthur <a@skatekrak.com>
2020-02-06 10:53:39 +0000

    :lipstick: Set full screen height on Page only for desktop

--- layouts/Page.tsx
+++ layouts/Page.tsx
@@ -18,18 +18,20 @@ export default Page;
  * Styled declarations
  */
 interface StyledProps {
+    // This prop allows us to put a fixed height on desktop
     isFullScreen?: boolean;
 }
 
 const PageContainer = styled.div<StyledProps>`
     position: relative;
-    ${({ isFullScreen }) => (isFullScreen ? 'height: 100vh' : 'min-height: 100vh')};
     display: flex;
     flex-direction: column;
+    min-height: 100vh;
     padding: 1rem;
     background: ${({ theme }) => theme.colors.bg.pageGradient};
 
     ${media.laptop} {
+        ${({ isFullScreen }) => isFullScreen && 'height: 100vh'};
         padding: 2rem;
     }
 `;
@@ -37,7 +39,9 @@ const PageContainer = styled.div<StyledProps>`
 const InnerContainer = styled.div<StyledProps>`
     position: relative;
     flex-grow: 1;
-    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    padding: 1.5rem;
     background-color: ${({ theme }) => theme.colors.bg.prim};
     ${({ isFullScreen }) => isFullScreen && 'overflow: hidden'};
     z-index: 1;
