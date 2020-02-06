efa9d72710527a3cea2d3d5d2695579c01e9ec50
Arthur <a@skatekrak.com>
2020-02-06 11:00:31 +0000

    :construction: Creating basic LandingLayout

---
+++ layouts/landing/LandingGrid.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+import media from 'styles/media';
+
+const LandingGrid = () => (
+    <Grid>
+        <Title>Arthur Molinos portfolio</Title>
+    </Grid>
+);
+
+export default LandingGrid;
+
+/*
+ * Styled declarations
+ */
+const Grid = styled.main`
+    ${media.laptop} {
+        display: grid;
+        grid-template-columns: 40% 20% 40%;
+        grid-template-rows: 50% 50%;
+    }
+`;
+
+const Title = styled.h1`
+    font-weight: 700;
+    font-size: 1rem;
+    text-align: center;
+    color: ${({ theme }: any) => theme.colors.text.primary};
+    padding: 2rem;
+    background-color: aquamarine;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-import styled from 'styled-components';
-
+import LandingGrid from 'layouts/landing/LandingGrid';
 import Page from 'layouts/Page';
-import media from 'styles/media';
 
 const Index = () => (
-    <Page>
-        <Main>
-            <Title>Arthur Molinos portfolio</Title>
-        </Main>
+    <Page isFullScreen>
+        <LandingGrid />
     </Page>
 );
 
@@ -16,22 +12,3 @@ export default Index;
 /*
  * Styled declarations
  */
-const Main = styled.main`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
-
-const Title = styled.h1`
-    font-weight: 700;
-    font-size: 1rem;
-    text-align: center;
-    color: ${({ theme }: any) => theme.colors.text.primary};
-    padding: 2rem;
-    background-color: aquamarine;
-
-    ${media.laptop} {
-        font-family: sans-serif;
-    }
-`;
