126cb12de65d00e55c35703502e7400a33646003
Arthur <a@skatekrak.com>
2020-02-06 11:43:50 +0000

    :sparkles: Create LandingLayout grid components

--- layouts/landing/LandingGrid.tsx
+++ layouts/landing/LandingGrid.tsx
@@ -4,7 +4,10 @@ import media from 'styles/media';
 
 const LandingGrid = () => (
     <Grid>
-        <Title>Arthur Molinos portfolio on develop</Title>
+        <AboutMe>About me!</AboutMe>
+        <Tools>Tools I use!</Tools>
+        <References>Some of my references!</References>
+        <Contact>arthur.molinos@gmail.com</Contact>
     </Grid>
 );
 
@@ -14,18 +17,39 @@ export default LandingGrid;
  * Styled declarations
  */
 const Grid = styled.main`
+    flex-grow: 1;
+
     ${media.laptop} {
         display: grid;
         grid-template-columns: 40% 20% 40%;
         grid-template-rows: 50% 50%;
+        grid-template-areas:
+            'about tools references'
+            'about tools contact';
     }
 `;
 
-const Title = styled.h1`
-    font-weight: 700;
-    font-size: 1rem;
-    text-align: center;
-    color: ${({ theme }: any) => theme.colors.text.primary};
-    padding: 2rem;
-    background-color: aquamarine;
+const AboutMe = styled.div`
+    grid-area: about;
+    background-color: red;
+`;
+
+const Tools = styled.div`
+    display: none;
+    grid-area: tools;
+    background-color: violet;
+
+    ${media.laptop} {
+        display: block;
+    }
+`;
+
+const References = styled.div`
+    grid-area: references;
+    background-color: green;
+`;
+
+const Contact = styled.div`
+    grid-area: contact;
+    background-color: blue;
 `;
